e5264ba338880d75e76dc24fa40d6437b371bb29
thewillyP <williamsleepark@gmail.com>
2025-05-08 02:59:14 -0400

    t

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-   name='metaopt',
-   version='1.0',
-   description='A useful module',
-   author='Daniel Jiwoong Im',
-   author_email='ji641@nyu.edu',
-   packages=['metaopt'],  #same as name
-   #install_requires=['bar', 'greek'], #external packages as dependencies
+    name="metaopt",
+    version="1.0",
+    description="A useful module",
+    author="Daniel Jiwoong Im",
+    author_email="ji641@nyu.edu",
+    packages=find_packages(),  # Automatically finds metaopt and all submodules
 )
-
